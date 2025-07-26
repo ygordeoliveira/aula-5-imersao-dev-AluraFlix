25e361e9742e8e2d8454a652c7436f15b82b6719
ygordeoliveira <ygor90928@gmail.com>
2025-07-26 18:25:42 -0300

    JS file of project

---
+++ script.js
@@ -0,0 +1,17 @@
+function adicionarFilme() {
+  var filmeFavorito = document.getElementById("filme").value;
+  if (filmeFavorito.endsWith(".jpg")) {
+    listarFilmesNaTela(filmeFavorito);
+  } else {
+    console.error("Endereço de filme inválido");
+  }
+  document.getElementById("filme").value = "";
+}
+
+function listarFilmesNaTela(filme) {
+  console.log(filme);
+  var elementoFilmeFavorito = "<img src=" + filme + ">";
+  var elementoListasFilmes = document.getElementById("listaFilmes");
+  elementoListasFilmes.innerHTML =
+    elementoListasFilmes.innerHTML + elementoFilmeFavorito;
+}
